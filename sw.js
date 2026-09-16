6e44f1ec0bc4e362baac866f3d00408e1d82c563
ktc-soe <chegokibidula@gmail.com>
2026-09-16 16:01:21 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'kwc-mis-v1';
+const ASSETS = [
+  './kwc-mis.html',
+  './manifest.json',
+  './kwc-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      // Network first for API calls, cache first for assets
+      if (e.request.url.includes('script.google.com')) {
+        return fetch(e.request).catch(() => cached);
+      }
+      return cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      });
+    })
+  );
+});
